a22167662a42a19cec8495dcce8590785b644836
Katie2206 <katielynch2204@gmail.com>
2023-12-26 22:13:49 +0000

    Added CA3 Starter Code

---
+++ CA3_Question1.java
@@ -0,0 +1,17 @@
+import java.util.Stack;
+import java.util.Scanner;
+/**
+ *  Name:
+ *  Class Group:
+ */
+public class CA3_Question1
+{
+    public static void runSimulation()
+    {
+
+    }
+
+    public static void main(String[] args) {
+        runSimulation();
+    }
+}
