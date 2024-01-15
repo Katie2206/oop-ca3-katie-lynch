3a9660e05fb0dac0a3690eb73818d7e9814aa4c4
Katie2206 <katielynch2204@gmail.com>
2024-01-15 23:52:45 +0000

    Created Menu
    Created an option menu to display actions that users can select.
    Can choose to add or remove car(s) to a driveway

--- CA3_Question1.java
+++ CA3_Question1.java
@@ -8,10 +8,37 @@ public class CA3_Question1
 {
     public static void runSimulation()
     {
+        Scanner kbrd = new Scanner(System.in);
+        Stack<Integer> driveway = new Stack<Integer>();
+        Stack<Integer> street = new Stack<Integer>();
+
+        int choice = 0;
+
+        do{
+            menu();
+            choice = kbrd.nextInt();
+            kbrd.nextLine();
+            switch(choice){
+                case 1:{
+
+                }break;
+                case 2:{
+
+                }break;
+            }
+
+        }while(choice != 0);
 
     }
 
+    public static void menu(){
+        System.out.println("0. Exit");
+        System.out.println("1. Add Car To Driveway");
+        System.out.println("2. Remove Car From Driveway");
+    }
+
     public static void main(String[] args) {
+
         runSimulation();
     }
 }
