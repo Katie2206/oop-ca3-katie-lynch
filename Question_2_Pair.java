c9e2e2ec5afd4fbf434ed3ce0edaa82ac132ff54
Katie2206 <katielynch2204@gmail.com>
2024-01-30 00:35:42 +0000

    Created Pair Class
    Created pair class for question 2, added code to get the row and column in the 10x10 array

--- Question_2_Pair.java
+++ Question_2_Pair.java
@@ -1,3 +1,33 @@
 public class Question_2_Pair {
+    private int r;
+    private int c;
 
+    public Question_2_Pair(int r, int c) {
+        this.r = r;
+        this.c = c;
+    }
+
+    public int getR() {
+        return r;
+    }
+
+    public void setR(int r) {
+        this.r = r;
+    }
+
+    public int getC() {
+        return c;
+    }
+
+    public void setC(int c) {
+        this.c = c;
+    }
+
+    @Override
+    public String toString() {
+        return "Question_2_Pair{" +
+                "r=" + r +
+                ", c=" + c +
+                '}';
+    }
 }
