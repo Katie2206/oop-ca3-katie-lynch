73ff0bc0e972895e06cecc46c826b9c7d2e9c29a
Katie2206 <katielynch2204@gmail.com>
2024-02-06 01:59:59 +0000

    added arraylist, added while loop
    added arraylist to store identifiers that match the useDelimiter pattern
    added while loop to loop through the file and identify the line numbers, individual lines or code and store them in the map and then find any identifiers and store them in the arraylist

--- CA3_Question3.java
+++ CA3_Question3.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
@@ -16,8 +17,29 @@ public class CA3_Question3
         Scanner in = new Scanner(file);
         in.useDelimiter("[^A-Za-z0-9_]+");
         Map<Integer, String> fileLines = new HashMap<>();
+        ArrayList<String> identifiers = new ArrayList<>();
         int lineNum = 0;
         String line = "";
+        String findIdentifiers = "";
+
+        //while loop loops through file until there are no values left
+        while(in.hasNext()){
+            //counts lines in file
+            lineNum++;
+            //stores individual lines
+            line = in.nextLine();
+            //adds both the line number and the contents of each line into the map
+            fileLines.put(lineNum, line);
+
+            //looks for identifiers that match the useDelimiter pattern
+            findIdentifiers = in.next();
+            //adds identifiers to (currently temporary) arraylist
+            identifiers.add(findIdentifiers);
+        }
+
+        //Test to see if values store
+        System.out.println("Identifiers: " + identifiers);
+        System.out.println("File: " + fileLines);
     }
 
     public static void main(String[] args) throws FileNotFoundException {
