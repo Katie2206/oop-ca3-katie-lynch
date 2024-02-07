df5b94b452e7e032a943be426b1f5c2a8882bf71
Katie2206 <katielynch2204@gmail.com>
2024-02-07 20:54:33 +0000

    added while loops to read through file, stored lines, line numbers and identifiers and stored them to a map
    two while loops read through the file and scan for any identifiers and store them and the line number. if a line contains an identifier, the identifier and line number is added to the map

--- CA3_Question3.java
+++ CA3_Question3.java
@@ -1,9 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 /**
  *  Name:
@@ -15,34 +12,56 @@ public class CA3_Question3
     public static void readFile(String fileName) throws FileNotFoundException {
 
         File file = new File(fileName);
-        Scanner in = new Scanner(file);
-        in.useDelimiter("[^A-Za-z0-9_]+");
-        Map<Integer, String> fileLines = new HashMap<>();
-        ArrayList<String> identifiers = new ArrayList<>();
+        Scanner f = new Scanner(file);
+        Map<String, List<Integer>> matchedIdentifiers = new HashMap<String, List<Integer>>();
+        List<Integer> numList = new ArrayList<Integer>();
         int lineNum = 0;
+//        int[] lineNumTotal = {};
         String line = "";
+        String identifier = "";
 
-        String findIdentifiers = "";
-
-        //while loop loops through file until there are no values left
-        while(in.hasNext()){
-            //counts lines in file
+        //loops through entire file and stores line numbers and lines
+        while (f.hasNext()) {
             lineNum++;
-            //stores individual lines
-            line = in.nextLine();
-            //adds both the line number and the contents of each line into the map
-            fileLines.put(lineNum, line);
+            line = f.nextLine();
 
-            //looks for identifiers that match the useDelimiter pattern
-            findIdentifiers = in.next();
-            //adds identifiers to (currently temporary) arraylist
-            identifiers.add(findIdentifiers);
+            Scanner in = new Scanner(line);
+            in.useDelimiter("[^A-Za-z0-9_]+");
+            //loops through file to find any identifiers to match the delimiter
+            while (in.hasNext()) {
+                //stores identifiers
+                identifier = in.next();
+
+                //checks what lines have the identifier in them
+                if (line.contains(identifier)) {
+                    //adds line number onto the list of numbers
+                    numList.add(lineNum);
+                    //adds identifier and line numbers to map
+                    matchedIdentifiers.put(identifier, numList);
+                } else {
+                    //if the identifier hasn't been stored in the map yet, store it here
+                    matchedIdentifiers.put(identifier, numList);
+                }
+
+//                if (line.contains(identifier)) {
+//                    matchLine.put(lineNum, line);
+//                    System.out.println(matchLine);
+//                }
+//                System.out.println(matchedIdentifiers);
+            }
+//            System.out.println("line: "+line);
+//            System.out.println("lineNum: "+lineNum);
+//            System.out.println("identifiers: "+identifier + "\n");
+
         }
 
-        //Test to see if values store
-        System.out.println("Identifiers: " + identifiers);
-        System.out.println("File: " + fileLines);
+        //iterate through the map and output each identifier and the lines they occur on
+        Iterator<Map.Entry<String, List<Integer>>> iterator = matchedIdentifiers.entrySet().iterator();
+        while (iterator.hasNext()) {
+            Map.Entry<String, List<Integer>> outputMap = iterator.next();
 
+            System.out.println("Identifier: " + outputMap.getKey() + " == " + outputMap.getValue());
+        }
     }
 
     public static void main(String[] args) throws FileNotFoundException {
